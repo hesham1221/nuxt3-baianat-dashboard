a5f956c632122d0ab05b8c009e99696617876fb8
hesham <hesham@baianat.com>
2022-12-03 16:10:35 +0200

    init the starter

---
+++ README.md
@@ -0,0 +1,28 @@
+#  Baianat Nuxt 3 Dashboard Starter 
+
+
+# install packages
+
+```bash
+pnpm install  # npm install
+```
+
+or
+
+```bash
+yarn install
+```
+
+# run the application 
+
+
+```bash
+pnpm dev # npm run dev
+```
+
+or
+
+```bash
+yarn dev
+```
+
---
+++ src/components/layouts/default/AppFooter.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export default {
+  name: 'AppFooter',
+  computed: {
+    darkTheme() {
+      return this.$appState.darkTheme;
+    }
+  },
+  methods: {
+    footerImage() {
+      return this.$appState.darkTheme ? '/images/logo-white.svg' : '/images/logo-dark.svg';
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="layout-footer">
+    made with ❤️ by
+    <a class="flex align-items-center" href="https://www.baianat.com/" target="_blank">
+      <img alt="Logo" :src="footerImage()" height="20" class="ml-2">
+      <span class="font-medium ml-1">Baianat</span>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
